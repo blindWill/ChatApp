9b358a2e0e664c466405e15dfdbacce448991452
blindWill <andrey.grabnyuk@mail.ru>
2023-02-17 12:55:52 +0300

    Set Up Firebase

---
+++ app/src/main/java/com/example/chatapp/firebase/MessagingService.kt
@@ -0,0 +1,20 @@
+package com.example.chatapp.firebase
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService: FirebaseMessagingService() {
+
+    @Override
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+       // Log.d("FCM", "Token: $token")
+    }
+
+    @Override
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        //Log.d("FCM", "RemoteMessage: ${message.notifica/
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/ui/SignInFragment.kt
@@ -0,0 +1,42 @@
+package com.example.chatapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.chatapp.R
+import com.example.chatapp.databinding.FragmentSignInBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() =_binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+}
